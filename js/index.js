0109846b16a367e13a535e72b0740abba3a800ed
Developer-Dennis <denniskipkogei11@gmail.com>
2022-03-22 10:37:54 +0300

    portfolio website for pot moulder

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// var home = document.getElementById("home");
+// var projects=document.getElementById("projects");
+// document.querySelectorAll(".nav-link").forEach(navLink => {
+//     navLink.addEventListener('click',(e)=>{
+//         let clickedElement= e.target.textContent
+//         console.log(clickedElement)
+//     })
+// })
+
+// function showHome(){
+//     document.getElementById("home").addEventListener("click");
+//     projects.style.display = "none";
+//     home.style.display = "flex";
+// }
+
+
+// function showProjects(){
+//     document.getElementById("projects").addEventListener("click");
+//     projects.style.display = "block";
+//     home.style.display = "none";
+// }
